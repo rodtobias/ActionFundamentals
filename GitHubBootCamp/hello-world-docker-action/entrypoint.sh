f43df5325e4ad32e3af50e863f6b79d16e38ebe0
rodtobias <rfc360@yahoo.com>
2024-11-21 09:00:24 -0500

    Update entrypoint.sh
    [skip ci]

--- GitHubBootCamp/hello-world-docker-action/entrypoint.sh
+++ GitHubBootCamp/hello-world-docker-action/entrypoint.sh
@@ -6,6 +6,9 @@ GREETING="Hello, $INPUT_WHO_TO_GREET!"
 # Use workflow commands to do things like set debug messages
 echo "::notice file=entrypoint.sh,line=7::$GREETING"
 
+# example using what was done during class
+echo "hello, $INPUT_WHO_TO_GREET!"
+
 # Write outputs to the $GITHUB_OUTPUT file
 echo "time=$(date)" >>"$GITHUB_OUTPUT"
 
